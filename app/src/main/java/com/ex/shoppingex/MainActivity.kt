483d505b79e4771ef09e0b5ce095e70b062480b4
Randy <o1984531@gmail.com>
2022-05-06 00:47:17 +0800

    [Add] Add main container page

--- app/src/main/java/com/ex/shoppingex/MainActivity.kt
+++ app/src/main/java/com/ex/shoppingex/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ex.shoppingex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ex.shoppingex.databinding.ActivityMainBinding
+import com.ex.shoppingex.utility.FragmentUtil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mBinding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        this.mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
