3a758059c12bf0de16010abb297fcefce4433078
Randy <o1984531@gmail.com>
2022-05-07 01:23:38 +0800

    [Add] Add data model for shopping list and item

---
+++ app/src/main/java/com/ex/shoppingex/data/ShoppingItemInfo.kt
@@ -0,0 +1,23 @@
+package com.ex.shoppingex.data
+
+data class ShoppingItemInfo(val price:Int?,
+                            val martShortName:String?,
+                            val imageUrl:String?,
+                            val finalPrice:Int?,
+                            val martName:String?,
+                            val stockAvailable:Int?,
+                            val martId:Int?) {
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is ShoppingItemInfo) {
+            return false
+        }
+
+        return price == other.price
+                && martShortName.equals(other.martShortName)
+                && imageUrl.equals(other.imageUrl)
+                && finalPrice == other.finalPrice
+                && martName.equals(other.martName)
+                && stockAvailable == other.stockAvailable
+                && martId == other.martId
+    }
+}
