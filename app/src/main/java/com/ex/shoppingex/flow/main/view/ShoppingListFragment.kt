f88c4aa684f4a146870f0fa35fae56c7b6f86c1c
Randy <o1984531@gmail.com>
2022-05-08 01:27:53 +0800

    [Add] Add ViewModel to observer api result and list selection

--- app/src/main/java/com/ex/shoppingex/flow/main/view/ShoppingListFragment.kt
+++ app/src/main/java/com/ex/shoppingex/flow/main/view/ShoppingListFragment.kt
@@ -1,15 +1,18 @@
 package com.ex.shoppingex.flow.main.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ex.shoppingex.R
 import com.ex.shoppingex.databinding.FragmentShoppingListBinding
 import com.ex.shoppingex.data.ShoppingItemInfo
+import com.ex.shoppingex.flow.main.viewmodel.ShoppingListViewModel
 
 class ShoppingListFragment : Fragment() {
 
@@ -20,6 +23,7 @@ class ShoppingListFragment : Fragment() {
     }
 
     private lateinit var mBinding:FragmentShoppingListBinding
+    private lateinit var mViewModel: ShoppingListViewModel
     private lateinit var mShoppingListAdapter: ShoppingListAdapter
 
 
@@ -31,17 +35,28 @@ class ShoppingListFragment : Fragment() {
         this.mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_shopping_list, container, false)
 
         initView()
+        initData()
         return this.mBinding.root
     }
 
     private fun initView() {
         this.mBinding.rvShoppingList.apply {
+            mViewModel = ViewModelProvider(this@ShoppingListFragment).get(ShoppingListViewModel::class.java)
             layoutManager = LinearLayoutManager(context)
-            mShoppingListAdapter = ShoppingListAdapter(context)
+            mShoppingListAdapter = ShoppingListAdapter(context, mViewModel)
             adapter = mShoppingListAdapter
 
             setHasFixedSize(true)
         }
+    }
 
+    private fun initData() {
+        mViewModel.getShoppingList().observe(requireActivity()) {
+            this.mShoppingListAdapter.addItems(it)
+        }
+
+        mViewModel.obsSelectedShoppingItemInfo().observe(requireActivity()) {
+            Log.d(TAG, "Selected item = ${it.martName}")
+        }
     }
 }
