8cea836b389d1ac1c4538dae2c8364993e66e73c
Randy <o1984531@gmail.com>
2022-05-09 23:15:00 +0800

    [Add] Add a ShoppingInfoDetailViewModel and binding layout

---
+++ app/src/main/java/com/ex/shoppingex/flow/shopping_info_detail/viewmodel/ShoppingInfoDetailViewModel.kt
@@ -0,0 +1,22 @@
+package com.ex.shoppingex.flow.shopping_info_detail.viewmodel
+
+import android.view.View
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
+import com.ex.shoppingex.data.ShoppingItemInfo
+
+class ShoppingInfoDetailViewModel {
+
+    val clickListener = ObservableField<View.OnClickListener>()
+    var martId = MutableLiveData<String>()
+    val martName = MutableLiveData<String>()
+    val price = MutableLiveData<String>()
+    var shoppingItemInfo: ShoppingItemInfo? = null
+        set(itemInfo) {
+            field = itemInfo
+            martId.value = itemInfo?.martIdDispStr
+            martName.value =  itemInfo?.martNameDispStr
+            price.value = itemInfo?.finalPriceDispStr
+        }
+        get() = field
+}
